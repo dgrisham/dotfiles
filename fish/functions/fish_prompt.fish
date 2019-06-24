224385afcce4a089e9b2ad1aaff20c13362e86e5
David Grisham <dmgrisham@gmail.com>
2019-06-23 23:24:11 -0600

    remove fish, zsh is better

--- fish/functions/fish_prompt.fish
+++
@@ -1,3 +0,0 @@
-function fish_prompt --description 'Write out the prompt'
-    echo -e -n -s (set_color 9b003e) "$USER" (set_color 666666) @ (set_color 253d7a) (prompt_hostname) (set_color 666666) ':' (set_color fff42b) (prompt_pwd) (set_color normal) "\n\$ "
-end
--- fish/functions/vi_bindings.fish
+++
@@ -1,5 +0,0 @@
-function vi_bindings
-    for mode in insert default visual
-        bind -M $mode \cf forward-char
-    end
-end
